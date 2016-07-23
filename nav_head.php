2ddc9fab9f30e7a54d7c50148800db13e7170315
Karim Soliman <karim.solimman@gmail.com>
2016-07-24 01:15:06 +0200

    Navigation header

---
+++ nav_head.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">PUATimeTable</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php"><i class="fa fa-home fa-fw" aria-hidden="true"></i> Home<span class="sr-only">(current)</span></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-info fa-fw" aria-hidden="true"></i> Information<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="courses.php"><i class="fa fa-book fa-fw" aria-hidden="true"></i>&nbsp;Courses</a></li>
+            <li><a href="#"><i class="fa fa-users fa-fw" aria-hidden="true"></i>&nbsp;Instructors</a></li>
+            <li><a href="#"><i class="fa fa-map-marker fa-fw" aria-hidden="true"></i>&nbsp;Rooms</a></li>
+          </ul>
+        </li>
+      </ul>
+      <ul class='nav navbar-nav navbar-right'>
+      		<li><a href="#">About</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
