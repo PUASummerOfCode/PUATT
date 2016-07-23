8a414785987be2c97ca0d8ee89f9a51a50a059be
Karim Soliman <karim.solimman@gmail.com>
2016-07-24 01:14:26 +0200

    Style.php include all the links for the external css files

---
+++ style.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+<link rel="stylesheet" type="text/css" href="css/footer.css">
